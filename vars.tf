1e9a7c5ce38839018248cdc31cf1fd6bf90310bd
ewanemarc <70907653+ewanemarc@users.noreply.github.com>
2021-03-01 20:41:53 -0700

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "ami_id" {
+  default = "ami-066c82dabe6dd7f73"
+}
+
+variable "name" {
+  default = "JJTechTest"
+}
